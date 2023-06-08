fe05cfcb8e86119d5d5bada06d406cbf14311ec1
SophieNordell <113043401+SophieNordell@users.noreply.github.com>
2023-06-08 09:38:33 +0200

    first commit

---
+++ Screens/Detail.js
@@ -0,0 +1,52 @@
+import { Button, Text, View } from "react-native";
+import { useEffect } from "react";
+import React from "react";
+import { StyleSheet } from "react-native";
+import { useState } from "react";
+
+export default function Detail({ navigation, route }) {
+  const { title, description, id, setTodos, todos } = route.params;
+  const [currentDate, setCurrentDate] = useState("");
+
+  const deleteTodo = (id) => {
+    setTodos(
+      todos.filter((todo) => {
+        if (todo.id !== id) return true;
+      })
+    );
+    navigation.navigate("Home");
+  };
+
+  useEffect(() => {
+    navigation.setOptions({
+      title: title,
+    });
+  }, [route]);
+
+  useEffect(() => {
+    const date = new Date(id);
+    let day = date.getDate(); //Current Date
+    let month = date.getMonth() + 1; //Current Month
+    let year = date.getFullYear(); //Current Year
+    setCurrentDate(day + "/" + month + "/" + year + " ");
+  }, []);
+
+  return (
+    <View>
+      <Text style={{ paddingTop: 30, paddingBottom: 30 }}>{description}</Text>
+      <Button title="Delete" onPress={() => deleteTodo(id)} />
+
+      <Text style={{ paddingTop: 30 }}>{currentDate}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  DetailButton: {
+    color: "red",
+    fontSize: 30,
+  },
+  red: {
+    color: "red",
+  },
+});
